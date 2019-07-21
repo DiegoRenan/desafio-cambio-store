d496d2d790cd17bbf248f20394deb041993eb595
DiegoRenan <renan.ccomp@gmail.com>
2019-07-20 21:34:43 -0300

    settings to heroku

--- app/javascript/bundles/CambioStore/components/Comments.jsx
+++ app/javascript/bundles/CambioStore/components/Comments.jsx
@@ -8,10 +8,11 @@ export default class Comments extends React.Component {
     
     return (
       <div className="flexcontainer--right__comments">
-        <h1>Compare e compre online nas principais Casas de Câmbio do país.</h1>
+        <h2><strong>Compare e compre online nas <br />
+          principais Casas de Câmbio do país.</strong></h2>
         <h5>Realize seu câmbio online em menos de 2 minutos de maneira rápida, fácil e 100% segura. Receba suas moedas onde quiser.</h5>
         <Slider />
-        <h5>Mais de 100 mil clientes satisfeitos</h5>
+        Mais de 100 mil clientes satisfeitos
         <h5>Câmbio 100% seguro parceiro autorizado pelo Banco Central</h5>
         <h5>Maior diversidade de moedas</h5>
         
