43e5ce6914c38bab42434fbe9d92a7f2c96afe50
DiegoRenan <renan.ccomp@gmail.com>
2019-07-20 23:37:06 -0300

    deploy heroku

--- app/javascript/bundles/CambioStore/components/Comments.jsx
+++ app/javascript/bundles/CambioStore/components/Comments.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 
 import Slider from '../templates/Slider'
+import MedalIcon from '../images/medalha.png'
+import LockIcon from '../images/cadeado.png'
+import GloboIcon from '../images/globo.png'
 
 export default class Comments extends React.Component {
   
@@ -10,11 +13,41 @@ export default class Comments extends React.Component {
       <div className="flexcontainer--right__comments">
         <h2><strong>Compare e compre online nas <br />
           principais Casas de Câmbio do país.</strong></h2>
-        <h5>Realize seu câmbio online em menos de 2 minutos de maneira rápida, fácil e 100% segura. Receba suas moedas onde quiser.</h5>
+        
+        <p>Realize seu câmbio online em menos de 2 minutos de maneira rápida, 
+          fácil e 100% segura. Receba suas moedas onde quiser.</p>
+          
         <Slider />
-        Mais de 100 mil clientes satisfeitos
-        <h5>Câmbio 100% seguro parceiro autorizado pelo Banco Central</h5>
-        <h5>Maior diversidade de moedas</h5>
+
+        <div className="comment-footer">
+          <div className="comment-footer__images">
+            <img src={MedalIcon} width="12%"/>
+            <img src={LockIcon} width="10%"/>
+            <img src={GloboIcon} width="12%"/>
+          </div>
+
+          <div className="comment-footer__text">
+            <div>
+              Mais de <br />
+              <span className="span-1">100 mil</span><br />
+              clientes<br />
+              satisfeitos
+            </div>
+            
+            <div>
+              Câmbio 100% <span className="span-2">seguro</span><br />
+              parceiro <span className="span-2"> autorizado</span><br />
+              pelo <span className="span-2"> Banco Central</span>
+            </div>
+            
+            <div>
+              Maior <br />
+              <span className="span-1">diversidade</span><br />
+              de moedas
+            </div>
+          </div>
+
+         </div>
         
       </div>
     );
